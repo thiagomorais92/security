16fc5dc3a5864f02ad8883d4568a06af420b3e2f
Thiago Morais <araujodemorais.t@gmail.com>
2016-08-01 17:49:37 -0300

    subindo scripts das tabelas para começar a implementar authenticação via JDBC

---
+++ criacao das tabelas.sql
@@ -0,0 +1,26 @@
+﻿-- Database: deixando_seguro
+
+-- DROP DATABASE deixando_seguro;
+
+CREATE DATABASE deixando_seguro
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+
+
+create table users(
+	userId integer PRIMARY KEY,
+	username varchar(45) NOT NULL,
+	password varchar(45) NOT NULL,
+	enabled boolean not null default TRUE
+);
+
+create table user_roles(
+	user_roleId integer PRIMARY KEY,
+	userId integer NOT NULL,
+	role varchar(45) NOT NULL
+);
